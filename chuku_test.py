03e52ed07bf13dd367e9a5738218e7d6f5cab765
tabzhangjx <tabzhangjx@outlook.com>
2018-05-08 19:16:16 +0800

    a

---
+++ chuku_test.py
@@ -0,0 +1,59 @@
+import mysqldb
+import os
+import sys
+import numpy as np
+
+def connect:
+
+
+
+
+
+
+def got_list:
+
+
+
+
+
+
+def main_operates:
+
+
+
+
+
+def load_all:
+
+
+
+
+
+
+def load_one(a):
+
+
+
+
+
+
+
+def print_list:
+
+
+
+
+
+
+
+def close_:
+
+
+
+print("Wellcome to IAIR_DATABASE system, the database you are going to connect is PROB, type 'y' to continue, 'n' to quit:")
+a=input();
+if a==n:
+	exit();
+
+db=
+
